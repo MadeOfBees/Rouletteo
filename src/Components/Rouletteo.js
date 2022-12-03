c641dbb2b39b65bb454fb85e644d4bdb170750d4
Bee <xronsredstone@gmail.com>
2022-12-03 17:24:21 -0500

    Minimum Viable Product

--- src/Components/Rouletteo.js
+++ src/Components/Rouletteo.js
@@ -1,7 +1,33 @@
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
+
+
 function Rouletteo() {
+    function pickRand() {
+        var list = document.getElementById("screenText").value.split("\n");
+        alert(list[Math.floor(Math.random() * list.length)])
+    }
     return (
         <div>
-            <h1>-</h1>
+            <h1>Make a list here:</h1>
+            <Box sx={{ width: '100%' }}>
+                <Stack spacing={2} className="NoDoubtPut">
+                    <Item className="screenText"><TextField id="outlined-multiline-static" label="Input" multiline rows={20} defaultValue="" placeholder="Put your list here"></TextField></Item>
+                    <Item><Button onClick={pickRand}>Pick a random list entry</Button></Item>
+                </Stack>
+            </Box>
         </div>
     );
 }
