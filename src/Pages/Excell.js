7791fbc5164a03dbd27b0539a4783d0b5b00e000
Bee <xronsredstone@gmail.com>
2022-12-04 13:07:00 -0500

    it works but it's ugly...

--- src/Pages/Excell.js
+++ src/Pages/Excell.js
@@ -3,12 +3,13 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
+import Input from '@mui/material/Input';
 import Backdrop from '@mui/material/Backdrop';
 import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
 import Typography from '@mui/material/Typography';
 import { read, utils } from "xlsx";
+import Button from '@mui/material/Button';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -35,8 +36,9 @@ function Rouletteo() {
     const [open, setOpen] = React.useState(false);
     const handleClose = () => setOpen(false);
     const [output, setOutput] = React.useState();
+    const [input, setInput] = React.useState();
 
-    const pickRand = (e) => {
+    const importFiles = (e) => {
         e.preventDefault();
         if (e.target.files) {
             const reader = new FileReader();
@@ -46,22 +48,29 @@ function Rouletteo() {
                 const sheetName = workbook.SheetNames[0];
                 const worksheet = workbook.Sheets[sheetName];
                 const json = utils.sheet_to_json(worksheet);
-                console.log(json);
+                setInput(json);
             };
             reader.readAsArrayBuffer(e.target.files[0]);
         }
     }
-    
-    
+    function pickRand() {
+        let list = input ;
+        var rand = JSON.stringify(list[Math.floor(Math.random() * list.length)]);
+        console.log(rand);
+        setOpen(true);
+        setOutput(rand);
+    }
+
+
     return (
         <div>
             <h1>Make a list here:</h1>
             <Box sx={{ width: '100%' }}>
                 <Stack spacing={2} className="NoDoubtPut">
                     <Item className="screenText">
-                        <input className='fileInput' type="file" id="file" accept=".xlsx, "onChange={pickRand} />
+                        <Input className='fileInput' type="file" id="file" accept=".xlsx, " onChange={importFiles} />
+                        <Item><Button onClick={pickRand}>Pick a random list entry</Button></Item>
                     </Item>
-                    <Item><Button onClick={pickRand}>Pick a random list entry</Button></Item>
                 </Stack>
             </Box>
             <Modal
@@ -80,7 +89,7 @@ function Rouletteo() {
                         <Typography id="transition-modal-title" variant="h6" component="h2">
                             We have chosen:
                         </Typography>
-                        <Typography id="modal-modal-description" variant="h1" component="h3">
+                        <Typography id="modal-modal-description" className='Wrap'>
                             {output}
                         </Typography>
                     </Box>
